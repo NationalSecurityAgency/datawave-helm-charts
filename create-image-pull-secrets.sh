029217f00af04f2eec3ac1bdf61a49e5419fa224
Adam Lerman awlerma <awlerma@uwe.nsa.gov>
2024-01-05 14:06:49 -0500

    initial commit

---
+++ create-image-pull-secrets.sh
@@ -0,0 +1,33 @@
+USERNAME=$1
+PAT=$2
+
+
+BASE64ENCODEDTOKEN=`echo -n "${USERNAME}:${PAT}" | base64`
+
+
+read -r -d '' FILETEXT << EOF
+{
+    "auths":
+    {
+        "ghcr.io":
+            {
+                "auth":"${BASE64ENCODEDTOKEN}"
+            }
+    }
+}
+EOF
+
+FILE_ENCODED=`echo -n  "$FILETEXT" | base64 | tr -d '\n' `
+
+cat > ./ghcr-image-pull-secret.yaml <<- EOF
+kind: Secret
+type: kubernetes.io/dockerconfigjson
+apiVersion: v1
+metadata:
+  name: dockerconfigjson-ghcr
+  labels:
+    app: app-name
+data: 
+  .dockerconfigjson: ${FILE_ENCODED}
+EOF
+
