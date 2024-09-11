249aa73f01174a45fabd547ac717a51273efaf23
David Lambertson <david.lambertson@opialab.com>
2024-09-11 15:47:00 -0600

    updating build pkg script

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,33 +1,7 @@
-#Package charts
-mkdir -p ./umbrella/charts/
-find ./ -name "*.tgz"  -delete   
-cd ./
-
-linting_passed=true
-for chart in hadoop accumulo zookeeper ingest web; 
-do
-    echo "linting and packaging $chart..."
-    cd $chart 
-    lint_result=$(helm lint .)
-    if [[ $lint_result == *"ERROR"* ]]; then
-        echo $lint_result
-        linting_passed=false
-    fi
-    helm package .
-    cp *.tgz ../umbrella/charts/
-    cd ..
-    echo "---------------------"
-done
-
-if ! $linting_passed; then
-    echo "One or more charts failed linting, not continuing."
-    exit
-fi
-
-find ./ -name "*.tgz"  -exec cp {} umbrella/charts/ \;
-# Deploy umbrella chart
-cd umbrella;
-helm lint .
+find . -name "*.tgz" -delete
+cd datawave-monolith-umbrella
+helm dependency update
 helm package .
-
-cp *.tgz ..
+cd ../datawave-stack;
+helm dependency update
+helm package .
