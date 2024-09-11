1f592f0272e1c116d43d9f010fb1da9dc3893fcc
David Lambertson <david.lambertson@opialab.com>
2024-09-11 15:39:41 -0600

    updates to ignore and local test Windows

--- startLocalTest.ps1
+++ startLocalTest.ps1
@@ -1,7 +1,6 @@
 ﻿Write-Output "Pulling images down..."
 docker pull rabbitmq:3.11.4-alpine
 docker pull busybox:1.28
-docker pull ghcr.io/nationalsecurityagency/datawave/ingest-kubernetes:6.9.0-SNAPSHOT
 Write-Output "Setting up minikube..."
 minikube delete --all --purge
 minikube start --cpus 8 --memory 12000 --disk-size 20480
@@ -9,8 +8,6 @@ Write-Output "Loading rabbitmq into minikube..."
 minikube image load rabbitmq:3.11.4-alpine
 Write-Output "Loading busybox into minikube..."
 minikube image load busybox:1.28
-Write-Output "Loading ingest-kubernetes into minikube..."
-minikube image load ghcr.io/nationalsecurityagency/datawave/ingest-kubernetes:6.9.0-SNAPSHOT
 Write-Output "Enabling ingress in minikube..."
 minikube addons enable ingress
 minikube kubectl -- delete -A ValidatingWebhookConfiguration ingress-nginx-admission
@@ -23,26 +20,22 @@ if (Test-Path .\ghcr-image-pull-secret.yaml) {
 }
 
 Write-Output "Clearing out old tgz files..."
-mkdir .\datawave-stack\charts -Force
+# mkdir .\datawave-stack\charts -Force
 foreach($zip_item in (Get-ChildItem -Recurse -Include *.tgz)) {
     Remove-Item $zip_item.FullName
 }
 
-Write-Output "Packaging helm charts..."
-# Create the individual tar balls and copy them to datawave-stack
-foreach($folder in ("hadoop", "accumulo", "zookeeper", "ingest", "web")) {
-    Write-Output $folder
-    Set-Location $folder
-    helm lint .
-    helm package .
-    Copy-Item *.tgz ../datawave-stack/charts/
-    Set-Location ..
-}
+Set-Location datawave-monolith-umbrella
+helm dependency update
+helm package .
 
 Write-Output "Packaging and deploying datawave-stack..."
 # Deploy datawave-stack
-Set-Location datawave-stack
+Set-Location ../datawave-stack
+helm dependency update
 helm lint .
 helm package .
-helm install dwv datawave-system-3.40.0.tgz -f "values-testing.yaml"
+kubectl create secret generic certificates-secret --from-file=keystore.p12=certificates/keystore.p12 --from-file=truststore.jks=certificates/truststore.jks
+$PACKAGE = Resolve-Path "./datawave-system-*.tgz" | Select-Object -ExpandProperty Path
+helm install dwv $PACKAGE -f "values-testing.yaml"
 Set-Location ..
